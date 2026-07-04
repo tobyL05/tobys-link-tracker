25671f2985ae1f35a95a4fcefe95546799e615e6
Toby <toby.lau075@gmail.com>
2026-07-04 17:21:55 -0400

    fix

--- main.py
+++ main.py
@@ -56,5 +56,5 @@ def main(request: flask.Request):
         print(f"Invalid link {id}: {error}")
         return redirect(DEFAULT_URL)
 
-    notify(f"[{datetime.now()}] {link.description}")
+    notify(f"[{datetime.now()}] {link.label}")
     return redirect(link.url)
